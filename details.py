df13368be818b82f79c763de393cbdfbf659f51a
Lucinda-Budzwa <xihluke.budzwa@liberty.co.za>
2025-02-16 00:23:34 +0200

    Add files via upload
    This file contains of my solution for the "user details" task as part of my coursework. The program asks for the user's details (name, age, house number, and street name) then prints out these details in one sentence.

---
+++ details.py
@@ -0,0 +1,7 @@
+#This program collect the user's details and prints them in one complte sentence.
+_name_ = input("Enter your name: ")
+_age_ = input("Enter your age: ")
+house_number =input("Enter your house number: ")
+street_name =input("Enter your street name: ")
+
+print(f"This is {_name_}.She is {_age_} years old and lives at {house_number} on {street_name}.")
