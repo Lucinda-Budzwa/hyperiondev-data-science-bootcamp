ac07bda77b6c18c567673ef0c1c2268eef89ab7f
Lucinda-Budzwa <xihluke.budzwa@liberty.co.za>
2025-02-15 18:28:13 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+#What is your name?
+name = input("Enter your name:")
+print("Your name is:",name)
+#What is your age?
+age = input("Enter your age:")
+print("Your age is:",age)
+print("Hello, World")
