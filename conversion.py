6025b4dd011fecb54e4270ed110fa5ccd9485515
Lucinda-Budzwa <xihluke.budzwa@liberty.co.za>
2025-02-16 01:44:16 +0200

    Add files via upload
    This file demonstrates how to convert different types of data in Python.

---
+++ conversion.py
@@ -0,0 +1,22 @@
+#Declare the variables.
+num1 = 99.23 #float
+num2 = 23    #interger
+num3 = 150   #interger
+string1 = "100" #string
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(string1))
+
+#Convert variables
+num1 = int(num1) #Convert float to an integer
+num2 = float(num2) #Convert integer to float
+num3 = str(num3)   #Convert integer to string
+string1 = int(string1) #Convert string to integer
+
+#Print variables on separate lines
+print(num1)
+print(num2)
+print(num3)
+print(string1)
